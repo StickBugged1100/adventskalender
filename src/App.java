71d04a0243309685e684bffe91e2b2e86020f9be
StickBugged1100 <thefbiarelookingforme@gmail.com>
2023-12-08 10:50:44 +0100

    2023-12-08 10:50

--- src/App.java
+++ src/App.java
@@ -8,11 +8,27 @@ public class App {
             File myObj = new File("input.txt");
             Scanner myReader = new Scanner(myObj);
 
+            int tal = 0;
+
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.println(data);
+                int n = 0;
+                while (!Character.isDigit(data.charAt(n))) {
+                    n++;
+                }
+                char z = data.charAt(n);
+
+                n = 1;
+                while (!Character.isDigit(data.charAt(data.length() - n))) {
+                    n++;
+                }
+                char y = data.charAt(data.length() - n);
+
+                tal += Integer.parseInt(z + "" + y);
             }
             myReader.close();
+
+            System.out.println(tal);
 
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
