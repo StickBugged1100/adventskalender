7bc9b55d5dbab9959b641ea85a872a84d527f67f
StickBugged1100 <thefbiarelookingforme@gmail.com>
2023-12-08 10:09:41 +0100

    2023-12-08

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        try {
+            File myObj = new File("input.txt");
+            Scanner myReader = new Scanner(myObj);
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+    }
+    }
+}
